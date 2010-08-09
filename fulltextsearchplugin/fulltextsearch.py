48bbb063f8e5b6fbfb55e0167f9115f1ad7d4a3c
Mikael Frykholm <mikael.frykholm@cgi.com>
2010-08-09 14:47:00 +0000

    Skipping the Interface idea. See #51.
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@976 484df14f-75d2-4b3f-8ba0-490e05069a01

--- fulltextsearchplugin/fulltextsearch.py
+++ fulltextsearchplugin/fulltextsearch.py
@@ -9,40 +9,31 @@ from trac.versioncontrol.api import IRepositoryChangeListener
 from trac.core import ExtensionPoint
 from trac.resource import get_resource_name
 from pkg_resources import resource_filename
-
-from trac.test import Mock, MockPerm
-from trac.web.href import Href
-from trac.wiki.formatter import HtmlFormatter
-from trac.mimeview import Context
-from trac.core import Interface
+import re
 
 class FullTextSearchObject(dict):
-    possible_fields = ('id', 'author', 'changed', 'created', 'oneline', 'type', 
+    possible_fields = ('id', 'author', 'changed', 'created', 'oneline', 'realm', 
                        'tags', 'involved', 'body', 'popularity')
 
     def __init__(self, id, **kwargs):
         self.id = id
         self.__dict__.update(kwargs)
 
-class IFullTextSearchProvider(Interface):
-    """Extension point interface for components providing full text search 
-       objects. Html is permitted."""
-
-    def get_fulltext():
-        """Return a FullTextSearchObject."""
-
-
 class Backend(Queue):
     """
     """
-    def create(self, id, item):
-        self.put(('CREATE', id, item))
+
+    def create(self, item):
+        item.action = 'CREATE'
+        self.put(item)
         
     def modify(self, id, item):
-        self.put(('MODIFY', id, item))
+        item.action = 'MODIFY'
+        self.put(item)
     
     def delete(self, id, item):
-        self.put(('DELETE', id, item))
+        item.action = 'DELETE'
+        self.put(item)
         
 class FullTextSearch(Component):
     """Search all ChangeListeners and prepare the output for a full text 
@@ -53,93 +44,94 @@ class FullTextSearch(Component):
     
     def __init__(self):
         self.backend = Backend()
-
-##    def render_wiki_to_html_without_req(self, thing, wikitext):
-##        if wikitext is None:
-##            return ""
-##        try:
-##            req = Mock(
-##                href=Href(self.env.abs_href()),
-##                abs_href=self.env.abs_href,
-##                authname='anonymous', 
-##                perm=MockPerm(),
-##                chrome=dict(
-##                    warnings=[],
-##                    notices=[]
-##                    ),
-##                args={}
-##            )
-##            context = Context.from_request(req, thing.resource.realm, thing.resource.id)
-##            formatter = HtmlFormatter(self.env, context, wikitext)
-##            return formatter.generate(True)
-##        except Exception, e:
-##            raise
-##            self.log.error("Failed to render %s", repr(wikitext))
-##            self.log.error(exception_to_unicode(e, traceback=True))
-##            return wikitext    
         
+    def _unique_id(self, resource):
+        project_id = os.path.split(self.env.path)[1]
+        unique_id = u"%s.%s.%s"%(project_id, resource.realm, resource.id)
+        return unique_id
+    
     # ITicketChangeListener methods
     def ticket_created(self, ticket):
-        project_id = os.path.split(self.env.path)[1]
-        unique_id = u"%s.%s.%s"%(project_id,ticket.resource.realm,ticket.resource.id)
-        text = None
-#        text = self.render_wiki_to_html_without_req(ticket, ticket['description'])
-        text = ticket.resource.get_fulltext()
-        self.backend.create(unique_id, text)
-        self.log.debug("Ticket added for indexing: %s, %s"%(unique_id, text))
+        so = FullTextSearchObject(self._unique_id(ticket.resource))
+        so.author = ticket.values.get('reporter',None)
+        so.changed = ticket.values.get('changetime', None)
+        so.created = ticket.values.get('changetime', None)
+        so.realm = ticket.resource.realm
+        so.tags = ticket.values.get('keywords', None)
+        so.involved = 'cc' in ticket.values and re.split(r'[;,\s]+', ticket.values['cc'])
+        if not so.involved:
+            so.involved = so.author
+        so.popularity = 0 #FIXME
+        so.body = ticket.values
+        self.backend.create(so)
+        self.log.debug("Ticket added for indexing: %s %s"%(ticket,so))
 
     def ticket_changed(self, ticket, comment, author, old_values):
-        project_id = os.path.split(self.env.path)[1]
-        unique_id = u"%s.%s.%s"%(project_id,ticket.resource.realm,ticket.resource.id)
-        self.backend.modify(unique_id, ticket)
-        self.log.error("TICKET CHANGED!")
+        so = FullTextSearchObject(self._unique_id(ticket.resource))
+        so.changed = ticket.values.get('changetime', None)
+        so.tags = ticket.values.get('keywords', None)
+        so.involved = ()#FIXME
+        so.popularity = 0 #FIXME
+        self.backend.modify(self, so)
+        self.log.debug("Ticket changed; updating full text index: %s %s"%(ticket,so))
 
-    def ticket_deleted(ticket):
-        project_id = os.path.split(self.env.path)[1]
-        unique_id = u"%s.%s.%s"%(project_id,ticket.resource.realm,ticket.resource.id)
-        self.backend.delete(unique_id, ticket)
-        self.log.error("TICKET DELETED!")
+    def ticket_deleted(self, ticket):
+        so = FullTextSearchObject(self._unique_id(ticket.resource))
+        self.backend.delete(so)
+        self.log.debug("Ticket deleted; deleting from index: %s %s"%(ticket,so))
         
     #IWikiChangeListener methods
-    def wiki_page_added(page):
-        project_id = os.path.split(self.env.path)[1]
-        unique_id = u"%s.%s.%s"%(project_id,page.resource.realm,page.resource.id)
-        context = Context.from_request(req, ticket.resource)
-        out = format_to_html(self.env, context, ticket[name],
-                       escape_newlines=self.must_preserve_newlines)
-        self.backend.create(unique_id, out)
-        pass
+    def wiki_page_added(self, page):
+        so = FullTextSearchObject(self._unique_id(page.resource))
+        so.author = page.author
+        so.changed = page.time
+        so.created = page.time
+        so.realm = page.resource.realm
+        so.tags = None #FIXME
+        so.involved = () #FIXME
+        so.popularity = 0 #FIXME
+        self.backend.create(so)
+        self.log.debug("WikiPage created for indexing: %s %s"%(page, so))
+        
+    def wiki_page_changed(self, page, version, t, comment, author, ipnr):
+        so = FullTextSearchObject(self._unique_id(page.resource))
+        so.changed = page.t
+        so.tags = None #FIXME
+        so.involved = () #FIXME author change
+        self.backend.create(so)
+        self.log.debug("WikiPage changed; updating full text index: %s %s"%(page, so))
 
-    def wiki_page_changed(page, version, t, comment, author, ipnr):
-        pass
-
-    def wiki_page_deleted(page):
-        pass
-
-    def wiki_page_version_deleted(page):
+    def wiki_page_deleted(self, page):
+        so = FullTextSearchObject(self._unique_id(page.resource))
+        self.backend.delete(so)
+        
+    def wiki_page_version_deleted(self, page):
+        #We don't care about old versions
         pass
 
     def wiki_page_renamed(page, old_name): 
-        pass
+#        so = FullTextSearchObject(page.resource.get_unique_id())
+        #delete and create
+        pass #FIXME
 
     #IAttachmentChangeListener methods
-    def attachment_added(attachment):
+    def attachment_added(self, attachment):
         """Called when an attachment is added."""
         pass
 
-    def attachment_deleted(attachment):
+    def attachment_deleted(self, attachment):
         """Called when an attachment is deleted."""
         pass
 
-    def attachment_reparented(attachment, old_parent_realm, old_parent_id):
+    def attachment_reparented(self, attachment, old_parent_realm, old_parent_id):
         """Called when an attachment is reparented."""
         pass
     
     #IMilestoneChangeListener methods
-    def milestone_created(milestone):
+    def milestone_created(self, milestone):
         pass
 
-    def milestone_changed(milestone, old_values):
+    def milestone_changed(self, milestone, old_values):
         """
         `old_values` is a dictionary containing the previous values of the
         milestone properties that changed. Currently those properties can be
@@ -147,16 +139,16 @@ class FullTextSearch(Component):
         """
         pass
 
-    def milestone_deleted(milestone):
+    def milestone_deleted(self, milestone):
         """Called when a milestone is deleted."""
         pass
     
     #IRepositoryChangeListener methods
-    def changeset_added(repos, changeset):
+    def changeset_added(self, repos, changeset):
         """Called after a changeset has been added to a repository."""
         pass
 
-    def changeset_modified(repos, changeset, old_changeset):
+    def changeset_modified(self, repos, changeset, old_changeset):
         """Called after a changeset has been modified in a repository.
        
         The `old_changeset` argument contains the metadata of the changeset
