04cfdd94ea9657c743955c9c0ca2c778b642b8d7
Karl Barker <karl.barker@cgi.com>
2012-01-24 11:57:51 +0000

    Add action to ITicketChangeListener interface, update all implementing classes, and relevant method calls. See #1440
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@4942 484df14f-75d2-4b3f-8ba0-490e05069a01

--- fulltextsearchplugin/fulltextsearch.py
+++ fulltextsearchplugin/fulltextsearch.py
@@ -252,7 +252,7 @@ class FullTextSearch(Component):
         self.backend.create(so)
         self.log.debug("Ticket added for indexing: %s", ticket)
         
-    def ticket_changed(self, ticket, comment, author, old_values):
+    def ticket_changed(self, ticket, comment, author, old_values, action=None):
         self.ticket_created(ticket)
 
     def ticket_deleted(self, ticket):
